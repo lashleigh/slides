e98277fc6d93f4af297123d96c5f47afc46e6703
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-14 01:23:12 -0800

    I have partially integrated. It looks like I will have to delete the inner_slide and with it everything that is related. Notes, headers the entire database really.

--- public/javascripts/note.js
+++ public/javascripts/note.js
@@ -1,14 +1,9 @@
 var db;
 var res;
-var i;
-var j;
-var uiWidth;
-var uiHeight;
-var uiTop;
-var uiLeft;
-var slideWidth; 
-var slideHeight;
-var cylonOffset;
+var i, j;
+var uiWidth, uiHeight, uiTop, uiLeft;
+var slideWidth, slideHeight, cylonOffset;
+var paper;
 $(function() {
   db = openDatabase('documents', '1.0', 'Local document storage', 5*1024*1024);
   db.transaction( function (t) {
@@ -25,7 +20,10 @@ $(function() {
         current_slide = group_results.rows.item(i);
         $(".slides").append(
         '<div id="slide_'+current_slide.id+'" class="slide zoomed_in_slide">'+
+          '<div id="raphael_'+current_slide.id+'" class="raphael"> </div>'+
           '<div class="slide_inner creation_enabled"> </div>'+
+          '<textarea id="code_for_raphael_'+current_slide.id+'" class="code">Text for the textarea - why do you go away?</textarea>'+
+          '<div id="run_container"> <button class="run" type="button">Run</button> </div>'+
         '</div>');
         t.executeSql('SELECT * FROM notes WHERE visible=1 AND slide_id=?', [current_slide.id], function(t, results) { 
           var len = results.rows.length;
@@ -42,11 +40,7 @@ $(function() {
     });
   });
 
-    $("#slides").disableSelection();
-    $("#slides").sortable({
-      cursor: 'crosshair',
-      grid: [50, 20]
-    });
+  $("#slides").disableSelection();
   $(".note").livequery( function() {
     $(this).draggable({ 
       snap: ".note",
@@ -167,8 +161,19 @@ $(function() {
     updateDB($(this));
   });
 
+  $(".run").live("click", function() {
+    console.log($($(this).parentsUntil(".slides")[1]).find(".raphael").attr("id"));
+    var id = ($($(this).parentsUntil(".slides")[1]).find(".raphael").attr("id"));
+    set_canvas(paper, id);
+    localStorage.setItem('code_for_'+id, $("#code_for_"+id).val());
+    //$("#code_for_"+id).val(localStorage.getItem("code_for_"+id));
+  });
+  $("textarea").live("keyup", function(event) {
+    event.stopPropagation();
+  });
+
   $(document).keydown( function(e) {
-    if( $(e.srcElement).hasClass("edit_area")) { 
+    if( $(e.srcElement).hasClass("edit_area") || $(e.srcElement).hasClass("code")) { 
       handleEdit(e);
     } else {
       handleKeys(e); 
@@ -332,6 +337,7 @@ function setCurrent() {
   for( i = 2; i < $(".slide").length; i++) {
     $($(".slide")[i]).addClass("reduced far-future")
   }
+  make_raphael_canvas($(".current"));
 }
 
 function get_order_array() {
@@ -340,4 +346,67 @@ function get_order_array() {
     id_array.push(parseInt($($(".slide")[i]).attr("id").split("_")[1]));
   }
   localStorage.setItem('slide_order', JSON.stringify({order: id_array}));
+}
+
+/*function canvas_generator() {
+  for(i = 0; i < $(".slide").size(); i++) {
+    make_raphael_canvas($( $(".slide")[i]) );
+  }
+}*/
+
+function make_raphael_canvas(slide) {
+    var id = slide.find(".raphael").attr("id");
+    paper = Raphael(id, 900, 500);
+    get_code(id);
+    set_canvas(paper, id);
+}
+
+function set_canvas(paper, id) {
+  paper.clear();
+  try {
+    (new Function("paper", "window", "document", $("#code_for_"+id).val() ) ).call(paper, paper);
+  } catch (e) {
+    alert(e.message || e);
+  }
+}
+
+function get_code(id) {
+  if( localStorage.getItem('code_for_'+id)) {
+    $("#code_for_"+id).val(localStorage.getItem("code_for_"+id));
+  } else {
+    $("#code_for_"+id).val( 'demo1 = paper.circle(320, 240, 60).animate({fill: "#223fa3", stroke: "#000", "stroke-width": 80, "stroke-opacity": 0.5}, 2000);\n'+
+                    'demo1.node.onclick = function () {\n'+
+                    '    demo1.attr("fill", "red");\n'+
+                    '};\n\n'+
+                    'var st = paper.set(); \n'+
+                    'var st = paper.set(); \n'+
+                    'st.push( \n'+
+                    '  paper.rect(800, 300, 50, 50, 10), \n'+
+                    '  paper.circle(670, 100, 60) \n'+
+                    ');\n\n'+
+                    'st.animate({fill: "red", stroke: "#000", "stroke-width": 30, "stroke-opacity": 0.5}, 1000);');
+  }
+}
+
+function basic_move() {
+  startCircle = function () {
+    // storing original coordinates
+    this.ox = this.attr("cx");
+    this.oy = this.attr("cy");
+  },
+  moveCircle = function (dx, dy) {
+    // move will be called with dx and dy
+    this.attr({cx: this.ox + dx, cy: this.oy + dy, opacity: .5});
+  },
+  up = function () {
+    // restoring state
+    this.attr({opacity: 1});
+  };
+  startRect = function() {
+    this.ox = this.attr("x");
+    this.oy = this.attr("y");
+  }
+  moveRect = function(dx, dy) {
+    this.attr({x: this.ox + dx, y: this.oy + dy, opacity: .5});
+  }
 }
